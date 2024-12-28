bfc34c21c5936145da70c3568143b32a1f5626c5
John Groton <john.groton@mymail.champlain.edu>
2024-12-28 11:14:00 -0500

    starting to add text boxes

---
+++ ShotgunJump/Assets/Scripts/tutorialonecontroller.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tutorialonecontroller : MonoBehaviour
+{
+    [SerializeField] private GameObject tutorialOne;
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.gameObject.tag == "Player")
+        {
+            Debug.Log("das");
+            tutorialOne.SetActive(true);
+        }
+    }
+}
